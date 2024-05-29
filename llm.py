248139e3e8c623dbe0a810bdb5995190b4b0bc4b
Charles Chang <80582567+charles1046@users.noreply.github.com>
2024-05-29 22:39:34 +0800

    Reapply "Change LLM config to heroku vars"
    This reverts commit d52a01b27a61b4e382dd17c8d0e4a037c357ccc2.

--- llm.py
+++ llm.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 
 
 class LLM:
@@ -7,8 +8,8 @@ class LLM:
         self.config = ConfigParser()
         self.config.read("config.ini")
         self.llm = ChatGoogleGenerativeAI(
-            model=self.config["Gemini"]["model"],
-            google_api_key=self.config["Gemini"]["API_KEY"]
+            model=os.environ.get('model'),
+            google_api_key=os.environ.get('API_KEY')
         )
 
     def invoke(self, text: str):
